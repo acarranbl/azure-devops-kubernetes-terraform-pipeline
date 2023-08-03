c8c7bbbd8b649edd1699b9d1eacbf0b12f045e8c
acarranbl <77801687+acarranbl@users.noreply.github.com>
2023-08-02 23:25:10 -0500

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.56.0"
+    }
+  }
+}
+ 
+provider "azurerm" {
+  
+  features {}
+}
+
+
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group}_${var.environment}"
   location = var.location
